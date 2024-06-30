034d7c62425b5a891f59a52fa3c99bb589094ecc
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-06-30 12:42:16 +0930

    som learning

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,9 +2,15 @@
 import React from 'react';
 
 const Home = () => {
+
+  const items = ['Apple', 'Banana', 'Orange'];
+
+
+const itemList = items.map((items))
+
   return (
     <>
-
+Hello world
     </>
   );
 };
