53ff248185f2aac2593e00dd0e23c8838a47e42b
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-08-01 21:13:41 +0930

    updates

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,15 @@
 
-import { Box } from "@chakra-ui/react";
+import { Box, VStack } from "@chakra-ui/react";
 import { fetchUsers } from "./lib/data";
 import HomePage from "@/components/navbar/landingPage/homePage/homePage";
 const Home = () => {
 
   return (
     <>
+    <VStack h="100vh" px={["100px", "100px", "100px", "100px", "100px", "100px"]}>
+
     <HomePage/>
+    </VStack>
 
 
       
