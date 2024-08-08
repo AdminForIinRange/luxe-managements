cb6fa572b515ff0b6d95b2f7f866c557077927e1
Anjesh Bhattarai <91888685+AdminForIinRange@users.noreply.github.com>
2024-08-08 06:31:52 +0000

    mapping out
      const frontFliter = [
        {
          title: "Property Type",
          value: "House",
        },
        {
          title: "  Post Code",
          value: "5000",
        },
        {
          title: "Service",
          value: " Linen/Amenity",
        },
        {
          title: "  Currently listed",
          value: "      Not Listed",
        },
      ];

--- src/components/landingPage/homePage/homePage.jsx
+++ src/components/landingPage/homePage/homePage.jsx
@@ -29,7 +29,24 @@ const HomePage = () => {
     }); // Initialize AOS with desired options
   }, []);
 
-  const frontFliter = [{}];
+  const frontFliter = [
+    {
+      title: "Property Type",
+      value: "House",
+    },
+    {
+      title: "  Post Code",
+      value: "5000",
+    },
+    {
+      title: "Service",
+      value: " Linen/Amenity",
+    },
+    {
+      title: "  Currently listed",
+      value: "      Not Listed",
+    },
+  ];
 
   return (
     // add some functional content at the end of the banner
@@ -68,7 +85,7 @@ const HomePage = () => {
               mt={"70px"}
             >
               <Text
-                fontSize={["30px", "30px", "35px", "45px", "85px", "85px"]}
+                fontSize={["30px", "30px", "35px", "85px", "85px", "85px"]}
                 fontWeight={500}
                 textAlign={"center"}
                 fontFamily={"Poppins"}
@@ -81,7 +98,7 @@ const HomePage = () => {
                 Luxe Managements{" "}
               </Text>
               <Text
-                fontSize={["12px", "12px", "16px", "16px", "18px", "35px"]}
+                fontSize={["24px", "28px", "28px", "35px", "35px", "35px"]}
                 textAlign={"center"}
                 fontFamily={"Poppins"}
                 color={"white"}
@@ -170,123 +187,51 @@ const HomePage = () => {
               gap={"10px"}
               p={"10px"}
             >
-              <VStack
-                transition={"all 0.3s ease"}
-                w={"100%"}
-                h={"100%"}
-                _hover={{ bg: "gray.100" }}
-                align={"center"}
-                justify={"center"}
-                cursor={"pointer"}
-                rounded={"18px"}
-              >
-                <HStack>
-                  <Text
-                    textAlign={"center"}
-                    fontWeight={400}
-                    color={"gray.700"}
+              {frontFliter.map(({ title, value }, index) => (
+                <>
+                  <VStack
+                    key={title}
+                    transition={"all 0.3s ease"}
+                    w={"100%"}
+                    h={"100%"}
+                    _hover={{ bg: "gray.100" }}
+                    align={"center"}
+                    justify={"center"}
+                    cursor={"pointer"}
+                    rounded={"18px"}
                   >
-                    Property Type
-                  </Text>
-                  <Text fontSize={"25px"}>
-                    <RiArrowDropDownLine />
-                  </Text>
-                </HStack>
+                    <HStack>
+                      <Text
+                        textAlign={"center"}
+                        fontWeight={400}
+                        color={"gray.700"}
+                      >
+                        {title}
+                      </Text>
+                      <Text fontSize={"25px"}>
+                        <RiArrowDropDownLine />
+                      </Text>
+                    </HStack>
 
-                <Box>
-                  <Text fontWeight={700} fontSize={"28px"} color={"gray.600"}>
-                    House
-                  </Text>
-                </Box>
-              </VStack>{" "}
-              <Divider orientation="vertical" borderColor="gray.400" h="100%" />
-              <VStack
-                transition={"all 0.3s ease"}
-                w={"100%"}
-                h={"100%"}
-                _hover={{ bg: "gray.100" }}
-                align={"center"}
-                justify={"center"}
-                cursor={"pointer"}
-                rounded={"18px"}
-              >
-                <HStack>
-                  <Text
-                    textAlign={"center"}
-                    fontWeight={400}
-                    color={"gray.700"}
-                  >
-                    Post Code
-                  </Text>
-                  <Text fontSize={"25px"}>
-                    <RiArrowDropDownLine />
-                  </Text>
-                </HStack>
-                <Box>
-                  <Text fontWeight={700} fontSize={"28px"} color={"gray.600"}>
-                    5000
-                  </Text>
-                </Box>
-              </VStack>
-              <Divider orientation="vertical" borderColor="gray.400" h="100%" />
-              <VStack
-                transition={"all 0.3s ease"}
-                w={"100%"}
-                h={"100%"}
-                _hover={{ bg: "gray.100" }}
-                align={"center"}
-                justify={"center"}
-                cursor={"pointer"}
-                rounded={"18px"}
-              >
-                <HStack>
-                  <Text
-                    textAlign={"center"}
-                    fontWeight={400}
-                    color={"gray.700"}
-                  >
-                    Service
-                  </Text>
-                  <Text fontSize={"25px"}>
-                    <RiArrowDropDownLine />
-                  </Text>
-                </HStack>
-                <Box>
-                  <Text fontWeight={700} fontSize={"28px"} color={"gray.600"}>
-                    Linen/Amenity
-                  </Text>
-                </Box>
-              </VStack>
-              <Divider orientation="vertical" borderColor="gray.400" h="100%" />
-              <VStack
-                transition={"all 0.3s ease"}
-                w={"100%"}
-                h={"100%"}
-                _hover={{ bg: "gray.100" }}
-                align={"center"}
-                justify={"center"}
-                cursor={"pointer"}
-                rounded={"18px"}
-              >
-                <HStack>
-                  <Text
-                    textAlign={"center"}
-                    fontWeight={400}
-                    color={"gray.700"}
-                  >
-                    Currently listed
-                  </Text>
-                  <Text fontSize={"25px"}>
-                    <RiArrowDropDownLine />
-                  </Text>
-                </HStack>
-                <Box>
-                  <Text fontWeight={700} fontSize={"28px"} color={"gray.600"}>
-                    Not Listed
-                  </Text>
-                </Box>
-              </VStack>
-              <Divider orientation="vertical" borderColor="gray.400" h="100%" />
+                    <Box>
+                      <Text
+                        fontWeight={700}
+                        fontSize={"28px"}
+                        color={"gray.600"}
+                      >
+                        {value}
+                      </Text>
+                    </Box>
+                  </VStack>
+
+                  <Divider
+                    orientation="vertical"
+                    borderColor="gray.400"
+                    h="100%"
+                  />
+                </>
+              ))}
+
               <VStack
                 transition={"all 0.3s ease"}
                 w={"30%"}
@@ -301,7 +246,7 @@ const HomePage = () => {
               >
                 <Box>
                   <Text
-                    fontSize={["12px", "12px", "12px", "12px", "14px", "32px"]}
+                    fontSize={["20px", "24px", "24px", "32px", "32px", "32px"]}
                     cursor={"pointer"}
                   >
                     <FaSearch />
@@ -315,7 +260,7 @@ const HomePage = () => {
                 w={"100%"}
                 border={"none"}
                 variant="unstyled"
-                fontSize={["12px", "12px", "12px", "12px", "14px", "28px"]}
+                fontSize={["18px", "24px", "24px", "28px", "28px", "28px"]}
                 placeholder="Search a service you need..."
               />
 
@@ -326,7 +271,7 @@ const HomePage = () => {
                 _hover={{ bg: "teal.300", color: "white" }}
                 color={"teal.300"}
                 bg={"white"}
-                fontSize={["12px", "12px", "12px", "12px", "14px", "32px"]}
+                fontSize={["20px", "24px", "24px", "32px", "32px", "32px"]}
                 cursor={"pointer"}
               >
                 <FaSearch />
