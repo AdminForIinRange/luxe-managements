08482e1d435c11f9eabb1939d18afcbaeff60066
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-08-18 18:19:39 +0930

    add

--- src/components/landingPage/gettingStarted/gettingStarted.jsx
+++ src/components/landingPage/gettingStarted/gettingStarted.jsx
@@ -10,7 +10,7 @@ import { GoDotFill } from "react-icons/go";
 const GettingStarted = () => {
   return (
     <>
-    <HStack
+     <HStack
         justify={"center"}
         align={"start"}
         w={"100%"}
@@ -174,7 +174,7 @@ const GettingStarted = () => {
                   Learn more
                 </Text>{" "}
                 <Text fontSize={"30px"}>
-
+ 
                 </Text>
               </HStack>
             </HStack>
@@ -195,7 +195,7 @@ const GettingStarted = () => {
             p={"10px"}
           >
             <Box
-            
+        
               // data-aos="fade-right"
               bgPos={"center"}
               bgSize={"cover"}
@@ -220,152 +220,7 @@ const GettingStarted = () => {
 
             w={["390px", "390px", "550px", "550px", "550px", "550px"]}
           >
-    
-          </Box>
-        </HStack>
-      </HStack>
-
-      <HStack
-        px={["4%", "4%", "6%", "6%", "6%", "10%"]}
-        justify={"center"}
-        align={"center"}
-        w={"100%"}
-        h={"100%"}
-        mt={["105px", "105px", "105px", "105px", "155px", "155px"]}
-      >
-        <HStack
-          justify={[
-            "center",
-            "center",
-            "space-between",
-            "space-between",
-            "space-between",
-            "space-between",
-          ]}
-          align={"start"}
-          w={"100%"}
-          h={"100%"}
-          gap={["45px", "45px", "45px", "45px", "45px", "45px"]}
-          wrap={["wrap", "wrap", "nowrap", "nowrap", "nowrap", "nowrap"]}
-        >
-          <Box
-            display={["none", "none", "none", "block", "block", "block"]}
-            // data-aos="fade-right" // chnage dir
-            border={"5px solid rgb(255, 255, 255,0.25)"}
-            h={["600px", "600px", "600px", "600px", "600px", "600px"]}
-            w={["380px", "380px", "350px", "350px", "550px", "550px"]}
-            bg={"rgb(0,0,0,0.05)"}
-            backdropFilter="blur(1.5px)"
-            p={"10px"}
-            borderRadius={"30px"}
-            bgPos={"center"}
-            bgSize={"cover"}
-          >
-            <Box
-           
-              // data-aos="fade-right"
-              bgPos={"center"}
-              bgSize={"cover"}
-              w={"100%"}
-              h={"100%"}
-              p={"10px"}
-              borderRadius={"30px"}
-            ></Box>
-          </Box>
-          <Box w={["100%", "100%", "100%", "80%", "70%", "60%"]}>
-            <HStack
-              w={"100%"}
-              justify={[
-                "center",
-                "center",
-                "center",
-                "start",
-                "start",
-                "start",
-              ]}
-              align={"center"}
-              h={"100%"}
-              gap={["15px", "25px", "35px", "45px", "55px", "100px"]}
-            >
-              <HStack
-                // data-aos="fade-in"
-                rounded={"100px"}
-                justify={"start"}
-                align={"center"}
-                h={"100%"}
-                border={"1px solid rgb(255, 255, 255,0.25)"}
-                py={[1, 1, 2, 2, 2, 2]}
-              >
-                <Box>
-                  <GoDotFill color="black" fontSize={"20px"} />
-                </Box>
-                <Box>
-                  <Text
-                    fontFamily={"Poppins"}
-                    h={"100%"}
-                    textAlign={"center"}
-                    color={"black"}
-                    fontSize={["14px", "14px", "18px", "18px", "20px", "20px"]}
-                    fontWeight={500}
-                  >
-                    Feel at ease
-                  </Text>
-                </Box>
-              </HStack>
-            </HStack>
-
-            <Text
-              // data-aos="fade-in"
-              mt={["15px", "15px", "15px", "15px", "15px", "15px"]}
-              fontSize={["30px", "30px", "35px", "35px", "35px", "40px"]}
-              fontWeight={300}
-              fontFamily={"Poppins"}
-              bgClip="text"
-              textAlign={["center", "center", "center", "left", "left", "left"]}
-              color={"black"}
-              bgGradient="linear(to-r, green, red)"
-            >
-              Say Goodbye to Hosting Hassles
-            </Text>
-
-            <HStack
-              h={"100%"}
-              w={"100%"}
-              mt={["40px", "40px", "40px", "60px", "60px", "60px"]}
-              justify={"center"}
-              align={"start"}
-              gap={"50px"}
-              flexWrap={[
-                "wrap",
-                "nowrap",
-                "nowrap",
-                "nowrap",
-                "nowrap",
-                "nowrap",
-              ]}
-            >
-             
-            </HStack>
-
-            <HStack
-              h={"100%"}
-              w={"100%"}
-              mt={["40px", "40px", "40px", "60px", "60px", "60px"]}
-              justify={"center"}
-              align={"start"}
-              gap={["50px", "50px", "50px", "50px", "50px", "50px"]}
-              flexWrap={[
-                "wrap",
-                "nowrap",
-                "nowrap",
-                "nowrap",
-                "nowrap",
-                "nowrap",
-              ]}
-            >
-              
-            </HStack>
-          
+  
           </Box>
         </HStack>
       </HStack>
